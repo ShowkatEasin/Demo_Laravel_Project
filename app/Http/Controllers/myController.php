b4a2cbf39df7b8d45d396b86acdb309ff42e7750
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2023-01-08 21:21:27 +0600

    Routing

---
+++ app/Http/Controllers/myController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class myController extends Controller
+{
+    public function AboutPage(){
+        return "This is About Controller";
+    }
+    public function Calc(){
+        return 5*6;
+    }
+}
+    
